a3f3d696fc53ef6783e0c5071a84c9d6aff9372a
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-04-21 11:04:12 -0300

    Finalização de quizz implementada para layout desktop e mobile

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 // Tela 1 - Lista de quizzes
-const API = "https://mock-api.driven.com.br/api/v4/buzzquizz";
+const API = 'https://mock-api.driven.com.br/api/v4/buzzquizz';
 let idQuizzUsuario = 8107;
 
 obterQuizzes();
@@ -8,56 +8,56 @@ obterQuizzes();
 function obterQuizzes() {
   const promise = axios.get(`${API}/quizzes`);
   promise.then((response) => {
-    console.log("Consegui obter os quizzes da API!");
+    console.log('Consegui obter os quizzes da API!');
     separarQuizzesUsuario(response.data);
   });
   promise.catch(() => {
-    console.log("Não consegui obter os quizzes da API!");
+    console.log('Não consegui obter os quizzes da API!');
   });
 }
 
 function separarQuizzesUsuario(quizzes) {
-  let listaQuizzesTodos = "";
-  let listaQuizzesUsuario = "";
+  let listaQuizzesTodos = '';
+  let listaQuizzesUsuario = '';
 
   quizzes.forEach((el) => {
-    if(el.id !== idQuizzUsuario) {
+    if (el.id !== idQuizzUsuario) {
       listaQuizzesTodos += `
       <div class="quizz">
         <img src="${el.image}"/>
         <h6>${el.title}</h6>
       </div>
-      `;    
+      `;
     } else {
       listaQuizzesUsuario += `
       <div class="quizz">
         <img src="${el.image}"/>
         <h6>${el.title}</h6>
       </div>
-      `;  
+      `;
     }
   });
   renderizarListaQuizzes(listaQuizzesUsuario, listaQuizzesTodos);
 }
 
 function renderizarListaQuizzes(quizzesUsuario, quizzesTodos) {
-  console.log("Entrei em renderizar!");
-  const elUsuarioVazio = document.querySelector(".quizzes-usuario-vazio");
-  const elCabecalhoUsuario = document.querySelector(".cabecalho-quizzes-usuario");
-  const elQuizzesUsuario = document.querySelector(".quizzes-usuario");
-  const elQuizzesTodos = document.querySelector(".quizzes-todos");
+  console.log('Entrei em renderizar!');
+  const elUsuarioVazio = document.querySelector('.quizzes-usuario-vazio');
+  const elCabecalhoUsuario = document.querySelector('.cabecalho-quizzes-usuario');
+  const elQuizzesUsuario = document.querySelector('.quizzes-usuario');
+  const elQuizzesTodos = document.querySelector('.quizzes-todos');
 
   elQuizzesTodos.innerHTML = quizzesTodos;
 
-  if(quizzesUsuario !== "") {
-    elUsuarioVazio.classList.add("ocultar");
+  if (quizzesUsuario !== '') {
+    elUsuarioVazio.classList.add('ocultar');
     elQuizzesUsuario.innerHTML = quizzesUsuario;
-    elQuizzesUsuario.classList.remove("ocultar");
-    elCabecalhoUsuario.classList.remove("ocultar");  
-  } else if ((quizzesUsuario === "") && (elUsuarioVazio.classList.contains("ocultar") === true)) {
-    elUsuarioVazio.classList.remove("ocultar");
-    elQuizzesUsuario.classList.add("ocultar");
-    elCabecalhoUsuario.classList.add("ocultar");
+    elQuizzesUsuario.classList.remove('ocultar');
+    elCabecalhoUsuario.classList.remove('ocultar');
+  } else if (quizzesUsuario === '' && elUsuarioVazio.classList.contains('ocultar') === true) {
+    elUsuarioVazio.classList.remove('ocultar');
+    elQuizzesUsuario.classList.add('ocultar');
+    elCabecalhoUsuario.classList.add('ocultar');
   }
 }
 
@@ -67,9 +67,11 @@ function renderizarListaQuizzes(quizzesUsuario, quizzesTodos) {
 
 // function criarQuizz(el) {
 
-// } 
+// }
 
 // Tela 2 - Página de um quizz
+
+let nRespostasCorretas = 0;
 
 let exemploQuizz = {
   id: 8103,
@@ -141,9 +143,9 @@ let exemploQuizz = {
   ],
 };
 
-abreQuizz();
+abrirQuizz();
 
-function abreQuizz() {
+function abrirQuizz() {
   document.querySelector(
     '.pagina-quizz main .banner'
   ).style.background = `linear-gradient(0, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${exemploQuizz.image})`;
@@ -151,10 +153,10 @@ function abreQuizz() {
   document.querySelector('.pagina-quizz main .banner').style.backgroundSize = 'cover';
   document.querySelector('.pagina-quizz main .banner').innerHTML = `<span>${exemploQuizz.title}</span>`;
 
-  renderizaAsPerguntas(exemploQuizz.questions);
+  renderizarPerguntas(exemploQuizz.questions);
 }
 
-function renderizaAsPerguntas(questoes) {
+function renderizarPerguntas(questoes) {
   let perguntas = document.querySelector('.pagina-quizz main .perguntas');
   perguntas.innerHTML = '';
   questoes.forEach((questao) => {
@@ -163,13 +165,13 @@ function renderizaAsPerguntas(questoes) {
         <div class="titulo" style="background-color: ${questao.color}">
             <span>${questao.title}</span>
         </div>
-        <ul class="respostas">${renderizaAsRespostas(questao.answers)}</ul>
+        <ul class="respostas">${renderizarRespostas(questao.answers)}</ul>
     </li>`;
   });
 }
 
-function renderizaAsRespostas(respostas) {
-  const respostasEmbaralhadas = embaralhaRespostas(respostas);
+function renderizarRespostas(respostas) {
+  const respostasEmbaralhadas = embaralharRespostas(respostas);
   let respostasHtml = '';
   respostasEmbaralhadas.forEach((resposta) => {
     const corretaOuIncorreta = resposta.isCorrectAnswer ? 'correta' : 'incorreta';
@@ -182,7 +184,7 @@ function renderizaAsRespostas(respostas) {
   return respostasHtml;
 }
 
-function embaralhaRespostas(respostas) {
+function embaralharRespostas(respostas) {
   return respostas.sort(() => Math.random() - 0.5);
 }
 
@@ -194,14 +196,51 @@ function selecionarResposta(element) {
     resposta.classList.add('mostrar');
     if (resposta !== element) resposta.classList.add('opaca');
   });
-  rolarParaProximaPergunta();
+  checarSeRespostaEhCorreta(element);
+  setTimeout(rolarParaProximaPergunta, 2000);
+}
+
+function checarSeRespostaEhCorreta(element) {
+  if (element.classList.contains('correta')) nRespostasCorretas++;
 }
 
 function rolarParaProximaPergunta() {
-  setTimeout(() => {
-    const perguntaAtual = document.querySelector('.pergunta.selecionada');
-    const proximaPergunta = perguntaAtual.nextElementSibling;
-    if (proximaPergunta !== null) proximaPergunta.scrollIntoView({ block: 'center', behavior: 'smooth' });
-    perguntaAtual.classList.remove('selecionada');
-  }, 2000);
+  const perguntaAtual = document.querySelector('.pergunta.selecionada');
+  const proximaPergunta = perguntaAtual.nextElementSibling;
+  if (proximaPergunta !== null) {
+    proximaPergunta.scrollIntoView({ block: 'center', behavior: 'smooth' });
+  } else {
+    renderizarResultado();
+  }
+  perguntaAtual.classList.remove('selecionada');
+}
+
+function renderizarResultado() {
+  const nPerguntas = exemploQuizz.questions.length;
+  const percentualAcerto = Math.round((nRespostasCorretas / nPerguntas) * 100);
+  const nivel = definirNivel(percentualAcerto);
+  const resultadoHtml = document.querySelector('.pagina-quizz main .finalizacao');
+  resultadoHtml.innerHTML = `
+  <div class="titulo">
+    <span>${percentualAcerto}% de acerto: ${nivel.title}</span>
+  </div>
+  <div>
+    <img src="${nivel.image}">
+    <div class="texto">
+      <span>${nivel.text}</span>
+    </div>
+  </div>
+  `;
+  resultadoHtml.scrollIntoView({ block: 'center', behavior: 'smooth' });
+}
+
+function definirNivel(percentual) {
+  const indiceNivel = exemploQuizz.levels.findIndex((nivel) => percentual < nivel.minValue);
+  let nivelCorreto;
+  if (indiceNivel === -1) {
+    nivelCorreto = exemploQuizz.levels[exemploQuizz.levels.length - 1];
+  } else {
+    nivelCorreto = exemploQuizz.levels[indiceNivel - 1];
+  }
+  return nivelCorreto;
 }
